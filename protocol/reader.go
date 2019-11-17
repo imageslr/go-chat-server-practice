c15b50e9dcba9a5d0bd23c2212105ebc78240a92
zhukaihao <zhukaihao@bytedance.com>
2019-11-17 16:35:24 +0800

    feat: protocol
    Change-Id: Ia887fc8db490188da3b7d08162b58f0d2d8cae7c

--- protocol/reader.go
+++ protocol/reader.go
@@ -24,7 +24,38 @@ Read 从 r.reader 中读取一条命令
 interface{} 是 protocol/command 中定义的 XXXCommand 结构体
 */
 func (r *CommandReader) Read() (interface{}, error) {
-	// TODO
+	command, err := r.reader.ReadString(' ')
+	if err != nil {
+		return nil, err
+	}
+
+	command = command[:len(command)-1] // bufio.ReadString 方法返回的结果包含界定符，去掉
+
+	switch command {
+	case "SEND":
+		message, err := r.reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		return SendCommand{Message: message[:len(message)-1]}, nil
+	case "NAME":
+		name, err := r.reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		return NameCommand{Name: name[:len(name)-1]}, nil
+	case "MESSAGE":
+		name, err := r.reader.ReadString(' ')
+		if err != nil {
+			return nil, err
+		}
+		message, err := r.reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		return MessageCommand{Message: message[:len(message)-1], Name: name[:len(name)-1]}, nil
+	}
+
 	return nil, UnknownCommand
 }
 
@@ -32,6 +63,18 @@ func (r *CommandReader) Read() (interface{}, error) {
 ReadAll 从 r.reader 中读取所有命令。interface{} 同上
 */
 func (r *CommandReader) ReadAll() ([]interface{}, error) {
-	// TODO
-	return nil, nil
+	commands := []interface{}{}
+
+	for {
+		command, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return commands, err
+		}
+
+		commands = append(commands, command)
+	}
+
+	return commands, nil
 }
--- protocol/writer.go
+++ protocol/writer.go
@@ -5,6 +5,7 @@ CommandReader 将命令包装为字节码并写入到 io.Writer 中，这个 io.
 package protocol
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -23,6 +24,18 @@ Write 向 w.writer 中写入一条命令
 command 为 protocol/command 中的 XXXCommand 结构体，但最终写入的命令应该是符合格式约定的字符串
 */
 func (w *CommandWriter) Write(command interface{}) error {
-	// TODO
-	return nil
+	var err error
+
+	switch v := command.(type) {
+	case SendCommand:
+		_, err = w.writer.Write([]byte(fmt.Sprintf("SEND %s\n", v.Message)))
+	case MessageCommand:
+		_, err = w.writer.Write([]byte(fmt.Sprintf("MESSAGE %s %s\n", v.Name, v.Message)))
+	case NameCommand:
+		_, err = w.writer.Write([]byte(fmt.Sprintf("NAME %s\n", v.Name)))
+	default:
+		err = UnknownCommand
+	}
+
+	return err
 }
